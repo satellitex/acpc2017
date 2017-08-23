1315a963a21e383a6cf999220c5bfa23a2534a5f
shake3277 <syageen@gmail.com>
2017-08-23 18:21:25 +0900

    Add generator and validator.

---
+++ shake1/tests/generator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "testlib.h"
+
+using namespace std;
+
+#define CASE_NUM 10
+
+const long long MIN_N = 1LL;
+const long long MAX_N = (1LL<<63LL-1LL);
+
+void start(int id) {
+  ofstream out(format("50_random_%02d.in", id).c_str());
+  long long N = rnd.next(MIN_N, MAX_N);
+  out << N << endl;
+}
+
+int main(int argc, char* argv[]) {
+  registerGen(argc, argv, 1);
+  for(int t = 0; t < CASE_NUM; ++t) {
+    start(t);
+  }
+  return 0;
+}
---
+++ shake1/tests/validator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include "testlib.h"
+
+using namespace std;
+
+const long long MIN_N = 1LL;
+const long long MAX_N = (1LL<<63LL-1LL);
+
+long long N;
+
+void input() {
+  N = inf.readLong(MIN_N, MAX_N, "N");
+  inf.readEoln();
+  inf.readEof();
+}
+
+void check() {
+}
+
+int main() {
+  registerValidation();
+  input();
+  check();
+  return 0;
+}
