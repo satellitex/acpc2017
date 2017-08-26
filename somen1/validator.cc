e502d7524da9580fd3c0026008099a7f6d168e65
someso <kota@wlan0832.u-aizu.ac.jp>
2017-08-26 15:11:22 +0900

    add validator

--- somen1/validator.cc
+++ somen1/validator.cc
@@ -1,13 +1,21 @@
 #include "testlib.h"
 
 using namespace std;
-const int Theta_MIN = 0;
-const int Theta_MAX = 360;
+const int N_MIN = 1;
+const int N_MAX = 1000;
+const int X_MIN = 0;
+const int X_MAX = 360;
 
-int Theta;
+int N;
+int X[N_MAX];
 void input(){
-  Theta = inf.readInt(Theta_MIN,Theta_MAX,"Theta");
+  N = inf.readInt(N_MIN,N_MAX,"N");
   inf.readEoln();
+  for(int i=0;i<N;i++){
+    Theta = inf.readInt(X_MIN,X_MAX,format("X[%d]",i));
+    inf.readEoln();
+  }
+  inf.readEof();
 }
 
 int main(){
