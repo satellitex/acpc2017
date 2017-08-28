db3a5efb37268219c81b615de7941d50cea73c7e
c7c7c7c7 <c7c7.c7ll@gmail.com>
2017-08-28 20:52:20 +0900

    a

---
+++ c7c7/tests/generator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "testlib.h"
+
+using namespace std;
+
+#define CASE_NUM 20
+
+const int MAX_M = 26 ;
+const int N =1000 ;
+const int seed =1000000007;
+
+vector<string>v;
+void start(int id) {
+  ofstream out(format("50_random_%02d.in", id).c_str());
+  int M = rnd.next(1, MAX_M);
+  out << N << " " << M << endl;
+  for(int i = 0; i < N; i++) {
+
+    if(i%10==3&&rnd.next(0,seed)%2) for(int j=0;j<M;j++){
+      int point=rnd.next(0,seed)%(int)v.size();
+      out<<v[point]<<endl;
+      v.erase(v.begin()+point);
+    }
+
+    else {
+      string t="";
+      for(int j = 0; j < M ; j++){
+        char c=rnd.next(0, MAX_M)+'a';
+         out << c ;
+         t+=c;
+       }
+       v.push_back(reverse(t.begin(),t.end()));
+       cout<<endl;
+     }
+  }
+}
+
+int main(int argc, char* argv[]) {
+  registerGen(argc, argv, 1);
+  for(int t = 0; t < CASE_NUM; ++t) {
+    start(t);
+  }
+  return 0;
+}
