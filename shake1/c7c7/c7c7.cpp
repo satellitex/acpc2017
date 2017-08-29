7b8d4890421cb965d60077c19eb0e49114b63003
c7c7c7c7 <c7c7.c7ll@gmail.com>
2017-08-30 00:23:59 +0900

    a

--- shake1/c7c7/c7c7.cpp
+++ shake1/c7c7/c7c7.cpp
@@ -3,14 +3,12 @@ using namespace std;
 #define int long long
 int n;
 main(){
-	while(cin>>n,n){
-		for(int i=0;(1<<i)<n;i++){
-			if(!((1<<i)&n)){
-				cout<<i+1<<endl;
-				goto L;
-			}
+	cin>>n;
+	for(int i=0;(1<<i)<n;i++){
+		if(!((1<<i)&n)){
+			cout<<(1<<i)<<endl;
+			return 0;
 		}
-		cout<<(-1)<<endl;
-		L:;
 	}
+	cout<<(-1)<<endl;
 }
