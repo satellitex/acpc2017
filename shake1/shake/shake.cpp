3abddc4f6befb9b88b4c2b905976e08ac6b576c7
shake3277 <syageen@gmail.com>
2017-08-31 03:41:24 +0900

    solution

--- shake1/shake/shake.cpp
+++ shake1/shake/shake.cpp
@@ -1,15 +1,14 @@
-#include<iostream>
-#include<cmath>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-  long long n,tmp,cnt=0;
+  long long n,cnt=0;
   cin>>n;
-  tmp=n+1;
-  while(tmp%2==0)tmp/=2,cnt++;
-  if(tmp==1)cout<<-1<<endl;
-  else {
-      n=pow(2,cnt);
-      cout<<n<<endl;
-    }
+  n++;
+  while(n%2==0)n/=2,cnt++;
+  if(n==1)cout<<-1<<endl;
+  else{
+    n=pow(2,cnt);
+    cout<<n<<endl;
+  }
   return 0;
 }
