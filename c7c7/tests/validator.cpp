a76e6662a9728a464d4d9769d0edfc6081171b24
c7c7c7c7 <c7c7.c7ll@gmail.com>
2017-09-01 11:20:30 +0900

    a

--- c7c7/tests/validator.cpp
+++ c7c7/tests/validator.cpp
@@ -11,6 +11,9 @@ int main() {
 	if(N<1||1000<N||M<1||30<M)exit(1);
 	for(int i=0;i<N;i++){
 		cin>>s;
+		for(int j=0;j<M;j++){
+			if(s[j]<'a'||'z'<s[j])exit(1);
+		}
 		if(s.size()!=M)exit(1);
 	}
 }
