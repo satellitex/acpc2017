348bc80a3c84b34d8920ebc1dd57287bf6b787c8
beet-aizu <aki.bdash@gmail.com>
2017-09-03 11:30:42 +0900

    add beet1

---
+++ beet1/beet/beet.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+struct BipartiteMatching{
+  vector<vector<int> > G;
+  int V;
+  vector<int> match,used;
+  
+  BipartiteMatching(){}
+  BipartiteMatching(int V):V(V){init();}
+  
+  void init(){
+    for(int i=0;i<(int)G.size();i++) G[i].clear();
+    G.clear();
+    match.clear();
+    used.clear();
+    G.resize(V);
+    match.resize(V,-1);
+    used.resize(V,0);
+  }
+  
+  void add_edge(int u,int v){
+    G[u].push_back(v);
+    G[v].push_back(u);
+  }
+
+  bool dfs(int v){
+    used[v]=true;
+    for(int i=0;i<(int)G[v].size();i++){
+      int u=G[v][i],w=match[u];
+      if(w<0||(!used[w]&&dfs(w))){
+	match[v]=u;
+	match[u]=v;
+	return true;
+      }
+    }
+    return false;
+  }
+
+  int calc(){
+    int res=0;
+    //fill(match.begin(),match.end(),-1);
+    fill(used.begin(),used.end(),0);
+    for(int v=0;v<V;v++){
+      if(match[v]<0){
+	if(dfs(v)){
+	  res++;
+	}
+      }
+    }
+    return res;
+  }
+};
+
+
+signed main(){
+  int n;
+  cin>>n;
+  BipartiteMatching bm(n+1);
+  int ans=0;
+  for(int i=1;i<=n;i++){
+    int p;
+    cin>>p;
+    bm.add_edge(p,i);
+    ans+=bm.calc();
+    cout<<ans<<endl;
+  }
+  return 0;
+}
---
+++ beet1/tests/generator_small.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "testlib.h"
+
+using namespace std;
+
+#define CASE_NUM 40
+
+const int MAX_N = 100;
+
+void start(int id) {
+  ofstream out(format("10_small_%02d.in", id).c_str());
+  int N = rnd.next(1, MAX_N);
+  out << N << endl;
+  for(int i = 0; i < N; i++) {
+    out << rnd.next(0, i) << endl;
+  }
+}
+
+int main(int argc, char* argv[]) {
+  registerGen(argc, argv, 1);
+  for(int t = 0; t < CASE_NUM; ++t) {
+    start(t);
+  }
+  return 0;
+}
