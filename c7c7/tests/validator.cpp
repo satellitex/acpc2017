3f6c94dd304ef483662de380aa00b7ce7675cc11
c7c7c7c7 <c7c7.c7ll@gmail.com>
2017-09-03 18:02:36 +0900

    a

--- c7c7/tests/validator.cpp
+++ c7c7/tests/validator.cpp
@@ -2,17 +2,27 @@
 
 using namespace std;
 
-int N,M;
+int N , M ;
+
 string s;
+
 int main() {
-	cin>>N>>M;
-	if(N<1||1000<N||M<1||30<M)exit(1);
-	for(int i=0;i<N;i++){
-		cin>>s;
-		for(int j=0;j<M;j++){
-			if(s[j]<'a'||'z'<s[j])exit(1);
+
+	cin >> N >> M ;
+
+	if( N < 1 || 1000 < N || M < 1|| 30 < M ) exit(1) ;
+
+	for( int i = 0 ; i < N ; i++ ) {
+
+		cin >> s ;
+
+		for( int j = 0 ; j < M ; j++ ) {
+
+			if( s[j] < 'a' || 'z' < s[j] ) exit(1) ;
 		}
-		if(s.size()!=M)exit(1);
+
+		if( s.size() != M ) exit(1) ;
 	}
-	if(scanf("%d",&N)!=EOF)exit(1);
+
+	if( scanf("%d",&N) != EOF ) exit(1) ;
 }
