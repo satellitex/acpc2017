a79e6e34dbfea7b26030a5950048e53b8141efac
Takuma Kageyama <uku09unk@gmail.com>
2017-09-05 18:38:12 +0900

    add uku solution

--- rollman1/uku/uku.cpp
+++ rollman1/uku/uku.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -9,19 +11,26 @@ int main() {
   int a[MAX_N];
   cin >> N;
   for(int i = 0; i < N; i++) cin >> a[i];
-
-  int l = 0, r = 0;
+  vector<int> vec;
+  vec.push_back(a[0]);
   for(int i = 1; i < N; i++) {
-    if(a[i-1] < a[i]) {
-      if(r == 0) r++;
-      l++;
-      r--;
-    } else if(a[i-1] > a[i]) {
-      if(l == 0) l++;
-      l--;
-      r++;
-    }
+    if(a[i] != vec.back()) vec.push_back(a[i]);
   }
-  cout << l+r+1 << endl;
+  int m = vec.size();
+  int cnt = 0, ans = 1;
+  for(int i = 1; i < m; i++) {
+    if(vec[i-1] < vec[i]) {
+      cnt = 0;
+      while(i < m && vec[i-1] < vec[i]) cnt++, i++;
+      i--;
+    } else if(vec[i-1] > vec[i]) {
+      cnt = 0;
+      while(i < m && vec[i-1] > vec[i]) cnt++, i++;
+      i--;
+    }
+    ans = max(ans, cnt+1);
+  }
+  cout << ans << endl;
+
   return 0;
 }
