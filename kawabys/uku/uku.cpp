96b895c476d9105374415d64d7a434ffcaa235a1
Takuma Kageyama <uku09unk@gmail.com>
2017-09-08 16:30:41 +0900

    add uku solution

---
+++ kawabys/uku/uku.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_N 100000
+
+using ll = long long;
+
+int main() {
+  ll N, X, L;
+  ll a[MAX_N], d[MAX_N];
+  cin >> N >> X >> L;
+  for(int i = 0; i < N; i++) cin >> a[i] >> d[i];
+  if(L <= X) {
+    ll ans = 0;
+    for(int i = 0; i < N; i++) {
+      ll sum = (a[i]+(X-L)*d[i]+a[i]+(X-1)*d[i])*L/2;
+      ans = max(ans, sum);
+    }
+    cout << ans << endl;
+    return 0;
+  }
+  auto Sum1 = [&](int i, int y, int x) {return (a[i]+y*d[i]+a[i]+(y+x-1)*d[i])*x/2;};
+  ll asum[MAX_N+1] = {};
+  for(int i = 0; i < N; i++) asum[i+1] = asum[i]+Sum1(i, 0, X);
+  auto Sum2 = [&](int l, int r) {return asum[r]-asum[l];};
+  ll l = L%X, n = L/X;
+  ll ans = 0;
+  for(int i = 0; i <= N-n; i++) {
+    ll tmp1 = 0, tmp2 = 0, tmp3 = 0;
+    if(i > 0) tmp1 = Sum1(i-1, X-l, l);
+    if(i+n < N) tmp2 = Sum1(i+n, 0, l);
+    if(i > 0 && i+n < N) {
+      ll lb = 0, ub = l;
+      while(lb+1 < ub) {
+	ll mb = (lb+ub)/2;
+	if(Sum1(i-1, X-mb, mb)+Sum1(i+n, 0, l-mb) > tmp1) ub = mb;
+	else lb = ub;
+      }
+      tmp3 = Sum1(i-1, X-ub, ub)+Sum1(i+n, 0, l-ub);
+    }
+    ans = max(ans, Sum2(i, i+n)+max({tmp1, tmp2, tmp3}));
+    //cout << i << " " << tmp1 << " " << tmp2 << " " << tmp3 << " " << Sum2(i, i+n) << " " << ans << endl;
+  }
+  cout << ans << endl;
+  return 0;
+}
