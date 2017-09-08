0a0302859ac6de4998d1924c9bc2c6db440e403d
shake3277 <syageen@gmail.com>
2017-09-08 20:48:56 +0900

    tests

--- shake1/tests/gen_large.cpp
+++ shake1/tests/gen_large.cpp
@@ -10,7 +10,7 @@ const long long MIN_N = 1LL;
 const long long MAX_N = 20LL;
 
 void start(int id) {
-  ofstream out(format("50_large_%02d.in", id).c_str());
+  ofstream out(format("10_large_%02d.in", id).c_str());
   long long N = pow(2,rnd.next(MIN_N, MAX_N));
   long long M = rnd.next(MIN_N,MAX_N);
   while(M%2==0)M/=2;
