5f92487460f9c12882ef0200fe606fdaa6205d12
Yohei Shimmyo <qwpmb554@gmail.com>
2017-09-11 11:02:12 +0900

    [WIP]Creating validator

---
+++ shake1/tests/validator_rollman.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include "testlib.h"
+
+using namespace std;
+
+
+
+void input() {
+  /*
+  N = inf.readInt(MIN_N, MAX_N, "N");
+  inf.readEoln();
+  for(int i = 0; i < N; i++) {
+    a[i] = inf.readInt(MIN_A, MAX_A, format("a[%d]", i+1));
+    inf.readEoln();
+  }
+  inf.readEof();
+  */
+}
+
+void check() {
+}
+
+int main() {
+  registerValidation();
+  input();
+  check();
+  return 0;
+}
