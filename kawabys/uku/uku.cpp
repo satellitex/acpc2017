2a630daef2a86c73bbf33998381bbc48d49ca533
Takuma Kageyama <uku09unk@gmail.com>
2017-09-12 20:47:06 +0900

    remove uku_tle

--- kawabys/uku/uku.cpp
+++ kawabys/uku/uku.cpp
@@ -13,17 +13,19 @@ int main() {
   cin >> N >> X >> L;
   for(int i = 0; i < N; i++) cin >> a[i] >> d[i];
   auto Sum1 = [&](int i, int y, int x) {return (a[i]+y*d[i]+a[i]+(y+x-1)*d[i])*x/2;};
-  if(L <= X) {
+  if(L < 2*X) {
     ll ans = 0;
     for(int i = 0; i < N; i++) {
       ll tmp1 = 0, tmp2 = 0, tmp3 = 0;
-      tmp1 = Sum1(i, X-L, L);
-      if(i+1 < N) tmp2 = Sum1(i+1, 0, L);
+      tmp1 = Sum1(i, max(0ll, X-L), min(X, L));
+      if(i+1 < N) tmp1 += Sum1(i+1, 0, max(0ll, L-min(X, L)));
+      if(i+1 < N) tmp2 = Sum1(i+1, 0, min(X, L));
+      tmp2 += Sum1(i, max(0ll, X-(L-min(L, X))), max(0ll, L-min(L, X)));
       if(i+1 < N) {
 	ll lb = 0, ub = L;
 	while(lb+1 < ub) {
 	  ll mb = (lb+ub)/2;
-	  if(Sum1(i, X-mb, mb)+Sum1(i+1, 0, L-mb) > tmp1) ub = mb;
+	  if(Sum1(i, max(0ll, X-mb), min(X, mb))+Sum1(i+1, 0, min(X, L-mb)) > tmp1) ub = mb;
 	  else lb = mb;
 	}
 	tmp3 = Sum1(i, X-ub, ub)+Sum1(i+1, 0, L-ub);
@@ -38,6 +40,10 @@ int main() {
   for(int i = 0; i < N; i++) asum[i+1] = asum[i]+Sum1(i, 0, X);
   auto Sum2 = [&](int l, int r) {return asum[r]-asum[l];};
   ll l = L%X, n = L/X;
+  if(l == 0) {
+    n--;
+    l += X;
+  }
   ll ans = 0;
   for(int i = 0; i <= N-n; i++) {
     ll tmp1 = 0, tmp2 = 0, tmp3 = 0;
