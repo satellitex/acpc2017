b6fd126ad8845f696d258fd46b598c58f3a700d2
kzyKT <s1200008aizu@gmail.com>
2017-09-16 15:27:03 +0900

    change solution

--- shake1/kzyKT/kzyKT.cc
+++ shake1/kzyKT/kzyKT.cc
@@ -4,11 +4,10 @@ using namespace std;
 int main() {
   long long n;
   cin >> n;
-  for(int i=0; (1LL<<i)<=n; i++) {
+  for(int i=0;;i++) {
     if(n&(1LL<<i)) continue;
     cout << (1LL<<i) << endl;
     return 0;
   }
-  cout << -1 << endl;
   return 0;
 }
