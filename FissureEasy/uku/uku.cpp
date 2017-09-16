42b4b8fa1ba8beaad57860378e1d0ceb7e325591
Takuma Kageyama <uku09unk@gmail.com>
2017-09-16 13:40:18 +0900

    add uku solution

---
+++ FissureEasy/uku/uku.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_N 100
+
+int N;
+char A[MAX_N][MAX_N];
+
+int main() {
+  cin >> N;
+  for(int i = 0; i < N; i++)
+    for(int j = 0; j < N; j++)
+      cin >> A[i][j];
+  int cnt = 0;
+  for(int i = 0; i < N; i++) {
+    for(int j = 0; j < N; j++) {
+      if(A[i][j] == 'x') continue;
+      cnt++;
+      for(int y = i; y < N && A[y][j] == 'o'; y++) {
+	for(int x = j; x < N && A[y][x] == 'o'; x++) {
+	  A[y][x] = 'x';
+	}
+      }
+    }
+  }
+  cout << (cnt-4)/3+1 << endl;
+  return 0;
+}
