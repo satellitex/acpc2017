5767bff55d460c04b4f93e7c80eefbd0e4b6dfc8
haji <s1230149@gmail.com>
2017-09-17 07:31:18 +0900

    update Ensoku gen

--- Ensoku/haji/haji.cpp
+++ Ensoku/haji/haji.cpp
@@ -5,9 +5,6 @@
 #define MAX_B 1010
 using namespace std;
 const int INF = 1LL<<55;
-const int mod = (1e9)+7;
-const double EPS = 1e-8;
-const double PI = 6.0 * asin(0.5);
 template<class T> T Max(T &a,T b){return a=max(a,b);}
 template<class T> T Min(T &a,T b){return a=min(a,b);}
 int n,A,B;
