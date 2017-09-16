b7fb7a34e5b88fe3e5518d156ffcd21ec52bd99f
shot1107 <harinezumi.1107@gmail.com>
2017-09-16 15:44:06 +0900

    change shot.cpp

--- Ensoku/shot/shot.cpp
+++ Ensoku/shot/shot.cpp
@@ -13,19 +13,20 @@ using namespace std;
 typedef pair<int, int> Pii; 
 typedef pair<int, Pii> Pip;
 const int INF = 100000;
+const int MAX_N = 14;
 
 struct Shop {
   int a, b, c;
 };
 
 int N, A, B;
-int K[10];
-Shop data[10][100];
-int d[10][10];
-int min_cost[1<<10]; // cost[i][j] : iの状態で家に帰ったときの最小コスト
-bool used[1<<10][10];
-int dp1[1<<5][1001]; // 駄菓子屋０〜５までを使った時の最大満足度
-int dp2[1<<10][1001]; // 駄菓子屋６〜１０
+int K[MAX_N];
+Shop data[MAX_N][100];
+int d[MAX_N][MAX_N];
+int min_cost[1<<MAX_N]; // cost[i][j] : iの状態で家に帰ったときの最小コスト
+bool used[1<<MAX_N][MAX_N];
+int dp1[1<<MAX_N][1001]; // 駄菓子屋０〜５までを使った時の最大満足度
+int dp2[1<<MAX_N][1001]; // 駄菓子屋６〜１０
 
 void calc_cost() {
   priority_queue<Pip, vector<Pip>, greater<Pip> > q; // fr : cost, sc.fr : bit, sc.sc : node
@@ -59,7 +60,7 @@ signed main() {
 
   Rep(i, N) Rep(j, N) cin >> d[i][j];
 
-  fill_n(min_cost, 1<<10, INF);
+  fill_n(min_cost, 1<<MAX_N, INF);
   memset(dp1, 0, sizeof(dp1));
   memset(dp2, 0, sizeof(dp2));
   
@@ -67,7 +68,7 @@ signed main() {
     
   int deq[1001];
   int deqv[1001];
-  Rep(b, 1<<5) Rep(i, 5) {
+  Rep(b, 1<<(MAX_N/2)) Rep(i, (MAX_N/2)) {
     if ( !((1<<i) & b) ) continue;
     Rep(j, K[i]) {
       int cost = data[i][j].a, satis = data[i][j].b, stock = data[i][j].c;
@@ -85,8 +86,8 @@ signed main() {
     }
   }
   
-  Rep(x, 1<<5) Rrep(i, 5, N) {
-    int b = x << 5;
+  Rep(x, 1<<(MAX_N/2)) Rrep(i, (MAX_N/2), MAX_N) {
+    int b = x << (MAX_N/2);
     if ( !((1<<i) & b) ) continue;
     Rep(j, K[i]) {
       int cost = data[i][j].a, satis = data[i][j].b, stock = data[i][j].c;
@@ -105,8 +106,8 @@ signed main() {
   }
   
   int ans = 0;
-  Rep(i, 1<<5) Rep(x, 1<<5) {
-    int j = x << 5;
+  Rep(i, 1<<(MAX_N/2)) Rep(x, 1<<(MAX_N/2)) {
+    int j = x << (MAX_N/2);
     int money = min(A-min_cost[i+j], B);
     if ( money < 0 || min_cost[i+j] == INF ) continue;
     Rep(k, money+1) {
